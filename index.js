b8bd5bf5f70e994a931eb3653b038c28a3609ff9
Juun <84310990+JuunKR@users.noreply.github.com>
2022-11-11 14:23:52 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -1,26 +1,16 @@
-$(document).ready(function () {
-    renderCurrentTime();
-    renderQuote();
-});
-
-
-function renderCurrentTime() {
-    let url = `https://worldtimeapi.org/api/timezone/Asia/Seoul`;
-    fetch(url)
-        .then(res => res.json()).then((data) => {
-            let datetime = data['datetime'].substr(11, 5);
-            $('#time').text(datetime);
-        });
+let clock = document.querySelector("#time")
+console.log(clock)
+function getTime() {
+    const time = new Date();
+    const hour = time.getHours();
+    const minutes = time.getMinutes();
+    const seconds = time.getSeconds();
+    clock.innerHTML = `${hour < 10 ? `0${hour}` : hour}:${minutes < 10 ? `0${minutes}` : minutes}`
 }
 
 
-function renderQuote() {
-    let url = `https://api.quotable.io/random`;
-    fetch(url)
-        .then(res => res.json()).then((data) => {
-            let content = `" ${data['content']} "`;
-            let author = `- ${data['author']} -`;
-            $('#content').text(content);
-            $('#author').text(author);
-        });
+function init() {
+    setInterval(getTime, 1000);
 }
+
+init();
